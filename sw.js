b7e4eddc212e81a0f3d19e2f7ca8911f56677fc1
mwveelen <maartenvanveelen@gmail.com>
2026-04-22 11:16:25 +0200

    pwd van gemaakt

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'tessa-todo-v1';
+const ASSETS = [
+  'index.html',
+  'style.css',
+  'app.js',
+  'manifest.json'
+];
+
+// Install service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch assets
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
